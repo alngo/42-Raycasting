ff2accf2182e72d5ac8f3d107104637ed1c67beb
alngo <alngo@student.42.fr>
2018-01-16 17:50:24 +0100

    floor

---
+++ src/floor.c
@@ -0,0 +1,20 @@
+#include "wolf3d.h"
+
+void			floor_cast(t_env *e, int x, int id)
+{
+	t_frgba		floor_color;
+	t_frgba		ceil_color;
+	int			y;
+
+	floor_color = ft_frgba(30, 30, 30, 0);
+	ceil_color = ft_frgba(80, 100, 255, 0);
+	if (e->line[id].end.y < 0)
+		e->line[id].end.y = HEIGHT;
+	y = e->line[id].end.y;
+	while (y < HEIGHT)
+	{
+		img_pixel_put(e, x, y, floor_color);
+		img_pixel_put(e, x, HEIGHT - y, ceil_color);
+		y++;
+	}
+}
